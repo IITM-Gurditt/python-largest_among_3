d90fa523e10fcd7c731bdb3e14470e8490a75959
IITM-Gurditt <153763191+IITM-Gurditt@users.noreply.github.com>
2023-12-13 23:36:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def find_largest_number(n1, n2, n3):
+  return max(n1,n2,n3)
+
+def main():
+  st.title("Find the Largest Number")
+
+  n1 = st.number_input("Enter the First Number:")
+  n2 = st.number_input("Enter the Second Number:")
+  n3 = st.number_input("Enter the Third Number:")
+
+  if st.button("Find Largest Number"):
+    largest = find_largest_number(n1, n2, n3)
+    st.success(f'The Largest number is: {largest}')
+
+if __name__ == "__main__":
+  main()
